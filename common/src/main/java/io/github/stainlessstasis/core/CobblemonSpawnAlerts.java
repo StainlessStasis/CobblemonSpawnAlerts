c3b933507bebee0dfd69f5a8d07d9a45bb7ad4c5
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-02-23 03:57:24 -0500

    Remove optional journeymap dependency from neo
    mandatory = false doesnt work???

--- common/src/main/java/io/github/stainlessstasis/core/CobblemonSpawnAlerts.java
+++ common/src/main/java/io/github/stainlessstasis/core/CobblemonSpawnAlerts.java
@@ -30,7 +30,7 @@ import java.util.stream.StreamSupport;
 
 public class CobblemonSpawnAlerts {
     public static final String MOD_ID = "cobblemon_spawn_alerts";
-    public static final String MOD_VERSION = "1.12.0";
+    public static final String MOD_VERSION = "1.12.1";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final CommonConfigManager COMMON_CONFIG_MANAGER = new CommonConfigManager();
     public static final String DEFAULT_POKEMON_CONFIG_NAME = "default (You can modify anything BELOW this, but dont delete it!)";
